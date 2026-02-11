2d005312f3480dd0ad935aca1a928d5c2c8d6196
EthanScherli <HE202488@students.ephec.be>
2026-02-11 19:07:06 +0100

    .5

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,16 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Serveur lancé sur http://localhost:${port}`);
+});
+
 function greet(name: string): string {
     return `Hello, ${name}!`;
 }
