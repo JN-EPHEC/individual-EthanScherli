8baecdf71464fc79adf31319ff2f658472d57679
EthanScherli <HE202488@students.ephec.be>
2026-02-17 16:45:36 +0100

    point 5.1

--- public/script.js
+++ public/script.js
@@ -9,8 +9,17 @@ async function fetchUsers() { // utilisation de async pour pouvoir utiliser awai
 
         users.forEach(user => {
             const li = document.createElement('li');
-            li.className = "list-group-item"; 
-            li.textContent = `${user.prenom} ${user.nom}`;
+            li.className ="list-group-item d-flex justify-content-between align-items-center";
+            const textSpan = document.createElement('span');
+            textSpan.textContent = `${user.prenom} ${user.nom}`;;
+
+            const deleteBtn = document.createElement('button');
+            deleteBtn.textContent = "X";
+            deleteBtn.className = "btn btn-danger btn-sm"; 
+            deleteBtn.onclick = () => deleteUser(user.id);
+
+            li.appendChild(textSpan);
+            li.appendChild(deleteBtn);
             userList.appendChild(li);
         });
     } catch (error) {
@@ -38,5 +47,16 @@ form.addEventListener('submit', async (event) => {
         console.error("Erreur lors de l'ajout :", error);
     }
 });
+
+async function deleteUser(id) {
+    if(!confirm("Êtes-vous sûr ?")) return; // Petite sécurité
+
+    try {
+        await fetch(`/api/users/${id}`, { method: 'DELETE' });
+        fetchUsers(); // Important : on recharge la liste après !
+    } catch (error) {
+        console.error("Erreur effacement", error);
+    }
+}
 
 fetchUsers();
