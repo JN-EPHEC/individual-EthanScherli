c8dfaa8bb7f732bead8fe62f73a6a9a3b16090ce
EthanScherli <HE202488@students.ephec.be>
2026-02-17 15:25:22 +0100

    point 4.1

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,7 @@ app.use(express.json())
 const port = 3000;
 
 app.use('/api/users', userRouter);
+app.use(express.static('public'));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
