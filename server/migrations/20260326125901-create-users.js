0c7a208c48b9e104322dc011bf0f8220e5879eec
EthanScherli <HE202488@students.ephec.be>
2026-03-26 14:45:41 +0100

    Debut TP05 ajout de la migration du dockerfile et changement dans le gitignore

---
+++ server/migrations/20260326125901-create-users.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      prenom: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      nom: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('Users');
+  }
+};
